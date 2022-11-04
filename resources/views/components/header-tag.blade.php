9f9fd38fad9a563b31e8b817e186ae90709d827c
Ryan <ryankinotikathurima@gmail.com>
2022-11-04 14:04:45 +0300

    major changes

---
+++ resources/views/components/header-tag.blade.php
@@ -0,0 +1,42 @@
+<meta charset="UTF-8">
+<meta name="viewport"
+      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Woodside Academy</title>
+
+{{--css--}}
+<link rel="stylesheet" href="css/style.css">
+
+{{--scripts--}}
+<script src="https://cdn.tailwindcss.com"></script>
+<script>
+    tailwind.config = {
+        theme: {
+            colors: {
+                'blue': '#1fb6ff',
+                'purple': '#7e5bef',
+                'pink': '#ff49db',
+                'orange': '#ff7849',
+                'green': '#13ce66',
+                'yellow': '#ffc82c',
+                'gray-dark': '#273444',
+                'gray': '#8492a6',
+                'gray-light': '#d3dce6',
+            },
+            fontFamily: {
+                sans: ['Graphik', 'sans-serif'],
+                serif: ['Merriweather', 'serif'],
+            },
+            extend: {
+                spacing: {
+                    '8xl': '96rem',
+                    '9xl': '128rem',
+                },
+                borderRadius: {
+                    '4xl': '2rem',
+                }
+            }
+        }
+    }
+</script>
+
