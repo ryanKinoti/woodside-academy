649ab576127e80a35f898497fac0c22dbec3ea3e
Makena-WB <102693481+Makena-WB@users.noreply.github.com>
2022-12-26 20:38:39 +0300

    dashboards and styling

--- resources/views/lecturers/dashboard.blade.php
+++ resources/views/lecturers/dashboard.blade.php
@@ -2,8 +2,72 @@
 <html lang="en">
 <head>
     <x-header-tag></x-header-tag>
+    <link rel="stylesheet" type="text/css" href="../css/lec-dashboard.css">
 </head>
 <body class="bg-gradient-to-r from-blue to-gray-op">
 <x-header></x-header>
+<section>
+<div class="sidebar">
+
+<div class="login-header">
+         <h1 id='login-title'> Woodside <br> Academy</h1>
+
+     
+     </div>
+
+<div class="sidebar-menu">
+<ul>
+       
+<li>
+ <a href="#"><span class="las la-address-book"></span>
+   <span>Home</span>
+ </a>
+</li>
+
+<li>
+ <a href="../Application/view_student_application.php"><span class="las la-address-book"></span>
+   <span>Lecturer's Application Details</span>
+ </a>
+</li>
+
+<li>
+ <a href="#">
+   <span class="las la-archive"></span>
+   <span>My Courses</span>
+ </a>
+</li>
+
+<li>
+ <a href="#">
+   <span class="las la-book"></span>
+   <span>Coursework material</span>
+ </a>
+</li>
+<li>
+ <a href="#"><span class="las la-user-friends"></span>
+   <span>Coursework Marks</span>
+ </a>
+</li>
+
+<li>
+
+ <a href="../lecturers/lecturer_details.php"><span class="las la-chalkboard-teacher"></span>
+   <span>Student attendance</span>
+ </a>
+</li>
+
+           <li>
+         <a href="#"><span><i class="fa fa-sign-out" aria-hidden="true"></i></span>
+             <span>Logout</span></a>
+     </li>
+
+
+</ul>
+</div>
+</div>
+<div class="image">
+     <img src="../school.jpg" alt="">
+ </div>
+</section>
 </body>
 </html>
