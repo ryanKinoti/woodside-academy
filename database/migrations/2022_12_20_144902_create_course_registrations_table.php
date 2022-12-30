b9f1c7b75416a7c5aecb5b8546596ace10242b0e
Ryan <ryankinotikathurima@gmail.com>
2022-12-31 01:08:04 +0300

    feat: able to add units and other major updates

---
+++ database/migrations/2022_12_20_144902_create_course_registrations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('course_registrations', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('course_id');
+            $table->bigInteger('lecturer_id');
+            $table->enum('registration_status',['completed','ongoing'])->default('ongoing');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('course_registrations');
+    }
+};
---
+++ resources/views/commons/notifications.blade.php
@@ -0,0 +1,3 @@
+<section
+    class="tabcontent mr-[1%] ml-[19%] my-space-0.3 w-v-w p-space-0.3 bg-green-op-2 rounded-xl"
+    id="notifications"></section>
