79a7afaf4fb82806a91420662b49c2449a4f9320
Ryan <ryankinotikathurima@gmail.com>
2022-12-30 22:06:30 +0300

    chore: completed admin sending messages

--- database/migrations/2022_12_20_144853_create_admin_messages_table.php
+++ database/migrations/2022_12_20_144853_create_admin_messages_table.php
@@ -18,6 +18,7 @@ return new class extends Migration {
             $table->bigInteger('from_user_id')->unsigned();
             $table->bigInteger('to_faculty_id')->unsigned()->nullable();
             $table->bigInteger('to_course_id')->unsigned()->nullable();
+            $table->enum('bulk_send', ['yes', 'no'])->default('no');
             $table->bigInteger('to_user_id')->unsigned()->nullable();
             $table->string('title');
             $table->string('message_content');
