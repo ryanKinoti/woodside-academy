ee58e5d79696b4d7a662e449d5056f99bcd9fb15
MeganDette <megan.mwago@gmail.com>
2023-01-01 19:09:48 +0300

    home page and application pages
    Design of the two pages

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,5 +5,18 @@
 </head>
 <body class="bg-gradient-to-r from-blue to-gray-op">
 <x-header></x-header>
+<div class="index-image">
+                <img src="{{asset('images/schoolindex.jpg')}}" alt="">
+                <div class="center">We Will Open The World Of Knowledge For You! <br> 
+                <p>Education is the passport to the future, for tomorrow belongs to those who prepare for it today.</p>
+            </div>
+</div>
+            <h1>About Woodside Academy</h1>
+            <p>Woodside Academy is an institution that has been known to perform well both in co-curricular and extra-curricular activities.In the co-curricular perspective it not only offers the common subjects offeredin all schools but in addition it offers coding classes,foreign languages,photography,art and design classes.While, in the extra-curricular activites the school offers different sports,dance classes and many more.The students grow having good habits and morals and achieve to be the number one best in the country. </p>
+<div class="img1">
+        <img src="{{asset('images/about1.jpg')}}" alt="">
+</div>
+
+</div>
 </body>
 </html>
