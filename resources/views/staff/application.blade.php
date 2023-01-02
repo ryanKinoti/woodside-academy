34bbe58febd134cb31d80451f7b814a79c1f4f2c
Makena-WB <102693481+Makena-WB@users.noreply.github.com>
2023-01-02 14:46:56 +0300

    staff application

--- resources/views/staff/application.blade.php
+++ resources/views/staff/application.blade.php
@@ -35,7 +35,7 @@
             </div>
         </form>
         <div class="staff-image">
-            <img src="{{asset('images/school2.jfif')}}" alt="">
+            <img src="{{asset('images/Brotherton Library.jfif')}}" alt="">
         </div>
     </div>
 </section>
