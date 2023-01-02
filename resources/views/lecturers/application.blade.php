8e34d8ce26cbfe4b8e2973cca00914f6ac790b6a
Ryan <ryankinotikathurima@gmail.com>
2023-01-02 11:42:14 +0300

    merge conflict 1

--- resources/views/lecturers/application.blade.php
+++ resources/views/lecturers/application.blade.php
@@ -8,53 +8,30 @@
 <x-register-header></x-register-header>
 <section>
     <div class="m-auto p-space-0.5 text-center">
-        <form action="/applications/choice/submission" method="POST"
-              class="grid items-center grid-rows-3 grid-flow-col gap-[3rem]">
+        <form action="/applications/choice/submission" method="POST" class="grid items-center">
             @csrf
-            <span class="my-space-0.2">
-                <label for="firstName">First Name :</label>
-                <input type="text" name="firstName" id="firstName" required autocomplete="off">
-            </span>
-
-            <span class="my-space-0.2">
-                <label for="lastName">Last Name :</label>
-                <input type="text" name="lastName" id="lastName" required autocomplete="off">
-            </span>
-
-            <span class="my-space-0.2">
-                <label for="phoneNo">Phone Number :</label>
-                <input type="text" name="phoneNo" id="phoneNo" required autocomplete="off">
-            </span>
-
-            <span class="my-space-0.2">
-                <label for="email">Personal Email :</label>
-                <input type="text" name="email" id="email" required autocomplete="off">
-            </span>
-
-            <span class="my-space-0.2">
-                <label for="gender">Gender :</label>
+            <div class="lecturer-form-container">
+                <input type="firstName" placeholder="First Name" name="firstName" id="firstName" required>
+                <input type="lastName" placeholder="Last Name" name="lastName" id="lastName" required>
+                <input type="phoneNo" placeholder="Phone No" name="phoneNo" id="phoneNo" required>
+                <input type="email" placeholder="Personal Email" name="email" id="email" required>
                 <select name="gender" id="gender">
                     <option value="male">Male</option>
                     <option value="female">Female</option>
                 </select>
-            </span>
-
-            <span class="my-space-0.2">
-                <label for="course">Select a Course to teach :</label>
                 <select name="course" id="course" class="w-fit">
                     @foreach($courses as $course)
                         <option value="{{$course->id}}">{{$course->course_name}}</option>
                     @endforeach
                 </select>
-            </span>
-
-            <div class="my-space-0.2">
                 <input type="hidden" name="role" value="{{$lecturerRole}}">
-                <input type="hidden" name="department" value="{{$department}}">
-                <input type="hidden" name="faculty" value="0">
-                <button type="submit">Complete Application</button>
+                <button id="reg-lec" type="submit">Complete Registration</button>
             </div>
+
         </form>
+        <div class="lecturer-image">
+            <img src="{{asset('images/applyforlec.jpg')}}" alt="">
+        </div>
     </div>
 </section>
 </body>
